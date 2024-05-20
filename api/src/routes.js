73f0fdf47e2e76af048ad3f9499721e65f628d23
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2024-05-20 15:07:15 -0300

    feat: import controllers

--- api/src/routes.js
+++ api/src/routes.js
@@ -3,6 +3,8 @@ const express = require("express");
 const router = express.Router();
 
 const destinos = require("./controller/destinos");
+const pontos = require("./controller/pontos");
+const hoteis = require("./controller/hoteis");
 
 const verify_connection = (req, res) => {
     res.send("Working Fine");
