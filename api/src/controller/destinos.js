569b34a1c049e4d0605ec5f622f7791889a9955f
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2024-05-20 09:46:38 -0300

    Destinos Controller

---
+++ api/src/controller/destinos.js
@@ -0,0 +1,24 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+const getAll = async (req, res) => {
+    const destinos = await prisma.destinos.findMany();
+    res.status(202).json(destinos).end();
+}
+
+const get = async (req, res) => {
+    const { id } = req.params;
+
+    const destinos = await prisma.destinos
+        .findUnique({
+            where: { id: Number(id) }
+        });
+
+    res.status(202).json(destinos).end();
+}
+
+module.exports = {
+    getAll,
+    get
+}
--- api/src/routes.js
+++ api/src/routes.js
@@ -2,10 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
+const destinos = require("./controller/destinos");
+
 const verify_connection = (req, res) => {
     res.send("Working Fine");
 }
 
 router.get("/", verify_connection);
+
+router.get("/destinos", destinos.getAll);
 
 module.exports = router;
